1b0a05ddf20bfb4807f8467ce170d55adf006f9d
André Nepomuceno <andre.nepomuceno@stone.com.br>
2022-11-28 22:10:30 -0300

    Added MouseHelper

---
+++ src/mouseHelper.js
@@ -0,0 +1,33 @@
+import { mouseToScreenCoord } from "./helpers";
+import { Vector2 } from "three";
+
+export class MouseHelper {
+    constructor() {
+        this.position = new Vector2();
+        this.velocity = new Vector2();
+        this.lastMove = Date.now();
+        this.overGUI = false;
+        this.samples = [];
+        this.taps = 10;
+    }
+
+    move(event) {
+        let pos = mouseToScreenCoord(event);
+
+        let now = Date.now();
+        let dt = now - this.lastMove;
+        this.lastMove = now;
+        this.velocity.set(pos.x - this.position.x, pos.y - this.position.y).divideScalar(dt / 1e3);
+
+        this.position.set(pos.x, pos.y);
+    }
+
+    addListener(domElement) {
+        domElement.addEventListener("mouseover", () => {
+            this.overGUI = true;
+        });
+        domElement.addEventListener("mouseleave", () => {
+            this.overGUI = false;
+        });
+    }
+}

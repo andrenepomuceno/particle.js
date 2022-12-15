1d235f9484267604ccd771ea3acc2abd930b8e64
Andre Nepomuceno <andrenepomuceno>
2022-12-15 00:12:30 -0300

    Added experimental computeShader2

--- src/gpu/shaders/computeShader.js
+++ src/gpu/shaders/computeShader.js
@@ -218,3 +218,178 @@ void main() {
     gl_FragColor = vec4(pos, type);
 }
 `;
+
+const computeVelocity2 = /* glsl */ `
+#include <common>
+
+precision highp float;
+
+uniform float minDistance2;
+uniform float massConstant;
+uniform float chargeConstant;
+uniform float nuclearChargeConstant;
+uniform float nuclearChargeRange;
+uniform float nuclearChargeRange2;
+uniform float forceConstant;
+uniform float boundaryDistance;
+uniform float boundaryDamping;
+uniform sampler2D textureProperties;
+
+const float width = resolution.x;
+const float height = resolution.y;
+
+#define UNDEFINED -1.0
+#define DEFAULT 0.0
+#define PROBE 1.0
+#define FIXED 2.0
+
+void main() {
+    vec2 uv1 = gl_FragCoord.xy / resolution.xy;
+
+    vec4 tPos1 = texture2D(texturePosition, uv1);
+    vec3 pos1 = tPos1.xyz;
+    float type1 = tPos1.w;
+    if (type1 == UNDEFINED) return;
+
+    vec4 props1 = texture2D(textureProperties, uv1);
+    float id1 = props1.x;
+    float m1 = props1.y;
+    vec4 tVel1 = texture2D(textureVelocity, uv1);
+    vec3 vel1 = tVel1.xyz;
+    float collisions = tVel1.w;
+
+    vec3 mField = vec3(0.0);
+    vec3 qField = vec3(0.0);
+    vec3 nqField = vec3(0.0);
+    vec3 cField = vec3(0.0);
+
+    for (float y = 0.0; y < height; y++) {
+        for (float x = 0.0; x < width; x++) {
+            vec2 uv2 = vec2(x + 0.5, y + 0.5) / resolution.xy;
+            vec4 tPos2 = texture2D(texturePosition, uv2);
+            vec3 pos2 = tPos2.xyz;
+            float type2 = tPos2.w;
+            if (type2 != DEFAULT && type2 != FIXED) continue;
+
+            vec4 props2 = texture2D(textureProperties, uv2);
+            float id2 = props2.x;
+            if (id1 == id2) {
+                continue;
+            }
+
+            float m2 = props2.y;
+            
+            vec3 dPos = pos2 - pos1;
+            float distance2 = dot(dPos, dPos);
+            
+            // check collision
+            if (distance2 <= minDistance2) {
+                if (type1 != PROBE) {
+                    vec3 vel2 = texture2D(textureVelocity, uv2).xyz;
+
+                    float m = m1 + m2; // precision loss if m1 >> m2
+                    if (m == 0.0) {
+                        continue;
+                    }
+
+                    float s = 2.0 * m1 * m2 / m;
+                    vec3 dVel = vel2 - vel1;
+                    if (distance2 > 0.0) {
+                        vec3 res = s * dot(dVel, dPos) * dPos;
+                        res /= distance2;
+                        cField += res;
+                    } else {
+                        cField += s * dVel;
+                    }
+                    
+                    ++collisions;
+                    continue;
+                } else {
+                    // for probe
+                    distance2 = minDistance2;
+                }
+            }
+
+            vec3 dPosU = normalize(dPos);
+            float q2 = props2.z;
+
+            #if !USE_DISTANCE1
+                mField += m2*dPosU/distance2;
+                qField += q2*dPosU/distance2;
+            #else
+                float distance1 = sqrt(distance2);
+                mField += m2*dPosU/distance1;
+                qField += q2*dPosU/distance1;
+            #endif
+
+            if (distance2 <= nuclearChargeRange2) {
+                float nq2 = props2.w;
+
+                #if !USE_DISTANCE1
+                    float distance1 = sqrt(distance2);
+                #endif
+
+                #if USE_HOOKS_LAW
+                    float x = -(2.0 * distance1 - nuclearChargeRange)/nuclearChargeRange;
+                #else
+                    float x = distance1/nuclearChargeRange;
+                    #if USE_POTENTIAL0 // "powXR"
+                        const float r = 1.0/3.0, log2 = log(2.0);
+                        x = pow(x, -log2 / log(r));
+                        x = sin(2.0 * PI * x);
+                    #elif USE_POTENTIAL1 // "exp"
+                        const float r1 = 1.0/3.0, r2 = 3.0, log2 = log(2.0);
+                        x = -exp(-log2 * x * r2 / r1);
+                        x = sin(2.0 * PI * x);
+                    #elif USE_POTENTIAL2 // "powAX"
+                        x = sin(7.22423 * (1.0 - pow(0.13026, x)));
+                    #else
+                        x = sin(2.0 * PI * x);
+                    #endif
+                #endif
+                nqField += nq2 * x * dPosU;
+            }
+        }
+    }
+
+    float q1 = props1.z;
+    float nq1 = props1.w;
+    vec3 rForce = vec3(0.0);
+    rForce += cField;
+    rForce += massConstant * m1 * mField;
+    rForce += -chargeConstant * q1 * qField;
+    rForce += nuclearChargeConstant * nq1 * nqField;
+    rForce *= forceConstant;
+
+    if (type1 == DEFAULT) {
+        if (m1 != 0.0) {
+            vel1 += rForce / abs(m1);
+        } else {
+            vel1 += rForce;
+        }
+        
+        #if ENABLE_BOUNDARY
+            // check boundary colision
+            vec3 nextPos = pos1 + vel1;
+            #if !USE_BOX_BOUNDARY
+                if (length(nextPos) >= boundaryDistance) {
+                    if (length(vel1) < boundaryDistance) {
+                        vel1 = boundaryDamping * reflect(vel1, normalize(pos1));
+                    } else {
+                        // particle will go out of boundaries
+                        vel1 = vec3(0.0);
+                    }
+                }
+            #else
+                if (abs(nextPos.x) >= boundaryDistance) vel1.x = -boundaryDamping * vel1.x;
+                if (abs(nextPos.y) >= boundaryDistance) vel1.y = -boundaryDamping * vel1.y;
+                if (abs(nextPos.z) >= boundaryDistance) vel1.z = -boundaryDamping * vel1.z;
+            #endif
+        #endif
+    } else if (type1 == PROBE) {
+        vel1 = rForce;
+    }
+
+    gl_FragColor = vec4(vel1, collisions);
+}
+`;

ab2ac0de519ece865698d5a7fd3c70e335d5dafa
Andre Nepomuceno <andrerm2k@hotmail.com>
2024-08-21 13:34:46 -0300

    Cleanup

--- madge.sh
+++ madge.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 
-killall webpack
-
 npm run madge0 &
 npm run madge1 &
 npm run madge2 &
 npm run madge-simple &
-
-npm run test &
-npm run record &
--- publish.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-git checkout -b release_v$1 $2 && git push origin release_v$1 $2 && git checkout dev

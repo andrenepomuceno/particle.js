06e3a334834443955abc945a83cf0d72adadc813
Andre Nepomuceno <andrerm2k@hotmail.com>
2024-09-07 06:45:15 -0300

    Tests with resizable

--- src/ui/DraggableDialog.jsx
+++ src/ui/DraggableDialog.jsx
@@ -1,9 +1,14 @@
 import React, { useState, forwardRef, useImperativeHandle } from 'react';
 import Draggable from 'react-draggable';
+import { ResizableBox } from 'react-resizable';
+
+import 'react-resizable/css/styles.css';
 import './DraggableDialog.css';
 
 const DraggableDialog = forwardRef(({ title, children }, ref) => {
     const [isOpen, setIsOpen] = useState(true);
+    const [width, setWidth] = useState(1);
+    const [height, setHeight] = useState(1);
 
     const handleCloseDialog = (e) => {
         setIsOpen(false);
@@ -16,16 +21,30 @@ const DraggableDialog = forwardRef(({ title, children }, ref) => {
     if (!isOpen) return null;
 
     return (
-        <Draggable handle=".dialog-header">
-            <div className="draggable-dialog">
-                <div className="dialog-header">
-                    <span>{title}</span>
-                    <button onClick={handleCloseDialog} className="close-button">
-                        &times;
-                    </button>
+        <Draggable
+            handle=".dialog-header"
+            defaultPosition={{ x: 100, y: 100 }}
+        >
+            {/* <ResizableBox
+                width={width}
+                height={height}
+                minConstraints={[100, 100]}
+                maxConstraints={[500, 500]}
+                onResize={(event, { size }) => {
+                    setWidth(size.width);
+                    setHeight(size.height);
+                }}
+            > */}
+                <div className="draggable-dialog">
+                    <div className="dialog-header">
+                        <span>{title}</span>
+                        <button onClick={handleCloseDialog} className="close-button">
+                            &times;
+                        </button>
+                    </div>
+                    <div className="dialog-content">{children}</div>
                 </div>
-                <div className="dialog-content">{children}</div>
-            </div>
+            {/* </ResizableBox> */}
         </Draggable>
     );
 });

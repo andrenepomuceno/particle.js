ea5e36ddd1d1ad44808643729321391b8414a5eb
Andre Nepomuceno <andrerm2k@hotmail.com>
2025-01-02 13:33:28 -0300

    Add GTAG to secrets

--- src/main.js
+++ src/main.js
@@ -22,7 +22,7 @@ function analytics() {
             window.dataLayer = window.dataLayer || [];
             function gtag() { dataLayer.push(arguments); }
             gtag('js', new Date());
-            gtag('config', 'G-STP92EN2LF');
+            gtag('config', ENV?.gtag_config);
         }
         new Promise(analytics);
     }
